461d1ee0eef0f3d3b6194d7230eb0a67198dbb3f
dominisz <dominisz@gmail.com>
2017-11-11 12:52:19 +0100

    Initial template

---
+++ Player.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            int N = in.nextInt(); // total number of players (2 to 4).
+            int P = in.nextInt(); // your player number (0 to 3).
+            for (int i = 0; i < N; i++) {
+                int X0 = in.nextInt(); // starting X coordinate of lightcycle (or -1)
+                int Y0 = in.nextInt(); // starting Y coordinate of lightcycle (or -1)
+                int X1 = in.nextInt(); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
+                int Y1 = in.nextInt(); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
+            }
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println("LEFT"); // A single line with UP, DOWN, LEFT or RIGHT
+        }
+    }
+}
